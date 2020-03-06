56f6283e95e9640aa53db9f3484a2b19881fb183
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-05 19:29:53 -0500

    implemented index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // Base: no items in array, return 0
+  if (!array.length) {
+    return 0;
+
+    // If first item is an array, call sumItems on it and sum the rest
+  } else if (Array.isArray(array[0])) {
+    return sumItems(array.shift()) + sumItems(array);
+
+    // If first item is number, add the number itself 
+    // and sum the rest of the array
+  } else {
+    return array.shift() + sumItems(array);
+  }
 }
 
 module.exports = sumItems;
